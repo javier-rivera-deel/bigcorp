4d4b1133b3c4ddb7ccda35ac72ded27513de9508
havebeeair <arq.javierriver@gmail.com>
2019-08-22 05:08:12 -0300

    Updates

---
+++ precache-manifest.5a685871aa6fdad61db44bb17c241079.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a4a84f4e7d8efece9d37a2904f5277",
+    "url": "/bigcorp/index.html"
+  },
+  {
+    "revision": "4290062f81934604f0dc",
+    "url": "/bigcorp/static/css/main.d3fca434.chunk.css"
+  },
+  {
+    "revision": "2cf2413e2c4b890e1cf5",
+    "url": "/bigcorp/static/js/2.e7a14e83.chunk.js"
+  },
+  {
+    "revision": "4290062f81934604f0dc",
+    "url": "/bigcorp/static/js/main.f090c8b1.chunk.js"
+  },
+  {
+    "revision": "9c52604000e90d8a5673",
+    "url": "/bigcorp/static/js/runtime~main.9e60ed59.js"
+  }
+]);
