6a781d680e165aa2c951541abdabf2fe2e776ab5
Javier Rivera <jrivera@secureauth.com>
2019-08-23 16:12:57 -0300

    removes old tree creation algorithm

--- src/Utils.js
+++ src/Utils.js
@@ -1,7 +1,3 @@
-export const getIds = tree => {
-	return tree.map(person => person.manager);
-};
-
 export const setEmployeeObject = employee => {
 	return {
 		name: employee.first + " " + employee.last,
@@ -21,80 +17,7 @@ export const findMiddlePosition = axis => {
 	}
 };
 
-export const getUniqueGenerations = generations => {
-	return generations
-		.sort((a, b) => b - a)
-		.reduce((unique, item) => {
-			return unique.includes(item) ? unique : [...unique, item];
-		}, []);
-};
-
-export const getSiblingGroups = (employeesList, uniqueGenerations) => {
-	const siblingGroups = [];
-	for (let generation in uniqueGenerations) {
-		const siblings = [];
-		for (let person in employeesList) {
-			if (uniqueGenerations[generation] === employeesList[person].manager) {
-				siblings.push(setEmployeeObject(employeesList[person]));
-			}
-		}
-		siblingGroups.push(siblings);
-	}
-	for (let group in siblingGroups) {
-		const currentGroup = siblingGroups[group];
-
-		// checks everygroup, another loop will be necesarry
-		if (currentGroup.length > 0) {
-			for (let sibling in currentGroup) {
-				// I COULD DO JUST A FOR LOOP
-				if (parseInt(group) < siblingGroups.length) {
-					const previous = siblingGroups[parseInt(group) + 1];
-
-					for (let child in previous) {
-						if (currentGroup.length > 0) {
-							if (
-								previous[child].attributes.department ===
-								currentGroup[sibling].attributes.department
-							) {
-								previous[child].children.push(currentGroup[sibling]);
-								currentGroup.splice(sibling, 1);
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	return siblingGroups;
-};
-
-export const createDataTree = siblingGroup => {
-	const parentIndex = siblingGroup.length - 1;
-	const directChildrenIndex = siblingGroup.length - 2;
-	const root = siblingGroup[parentIndex];
-	root[0].children = siblingGroup[directChildrenIndex];
-	return root;
-};
-
-// I COULD CREATE ARRAYS OF SIBLINGS THAT SHARES THE SAME MANAGER ID.
-// THOSE SIBLINGS WILL BE CHILDREN OF NODES WITH THE SAME ID.
-
-// LETS SAY I CREATE MY SIBLING GROUPS, STARTING WITH MY ROOT NODE
-const siblingGroups = [
-	{
-		managerId: 1,
-		siblings: []
-	},
-	{
-		managerId: 2,
-		siblings: []
-	},
-]
-// WITH THIS, I COULD LOOP OVER THIS GROUPS, AND ATTACH THEM AS CHILDREN IF THE ID MATCHES
-// BASICALLY I'M FINDING THE PARENT OF THE GROUP
-
-
-export const createDataTree1 = dataset => {
+export const createDataTree = dataset => {
   let hashTable = {};
   let lowestManager = dataset[0].manager;
 
@@ -110,12 +33,10 @@ export const createDataTree1 = dataset => {
     if(hashTable.hasOwnProperty(data.manager)) {
       hashTable[data.manager].children.push(hashTable[data.id]);
     } else if (data.manager === lowestManager || data.id === lowestManager) {
-          dataTree.push(hashTable[data.id]);
+        dataTree.push(hashTable[data.id]);
     } else {
     }
   });
-
-  // console.log(dataTree);
 
   return dataTree;
 };
--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,8 @@ import Tree from "react-d3-tree";
 
 import { nodeSize, treeStyle, foreignObject, tree } from "../consts";
 import {
-	getIds,
 	findMiddlePosition,
-	getUniqueGenerations,
-	getSiblingGroups,
-	createDataTree,
-	createDataTree1
+	createDataTree
 } from "../Utils";
 
 import "./App.css";
@@ -32,24 +28,11 @@ export default class App extends Component {
 		)
 			.then(res => res.json())
 			.then(employeesLists => {
-				// // GET MANAGER ID'S
-				// const unorderedTreeIDs = getIds(employeesList);
 
-				// // REMOVE DUPLICATES MANAGER ID'S AND, SORT THEM, REVERSE THEM
-				// const uniqueGenerations = getUniqueGenerations(unorderedTreeIDs);
-
-				// // CREATE GROUPS OF SIBLINGS - SIBLINGS SHARE SAME MANAGER ID
-				// const siblingGroups = getSiblingGroups(
-				// 	employeesList,
-				// 	uniqueGenerations
-				// );
-
-				// // CREATE DATA TREE TO FEED THE CHART
-				// const dataTree = createDataTree(siblingGroups);
-				const dataTree1 = createDataTree1(this.state.employeesList);
+				const dataTree = createDataTree(this.state.employeesList);
 
 				// // SET THE STATE :)
-				this.setState({ data: dataTree1, dataReady: true });
+				this.setState({ data: dataTree, dataReady: true });
 			});
 	}
 
